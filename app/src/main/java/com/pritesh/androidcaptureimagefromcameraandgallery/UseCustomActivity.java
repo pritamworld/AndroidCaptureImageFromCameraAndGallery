701ea3fdd6a7affa23f61af7be04032e6c34a8ed
Pritesh Patel <pritesh.patel@adesa.com>
2017-11-27 14:32:27 -0500

    cn.pedant.sweetalert:library added

--- app/src/main/java/com/pritesh/androidcaptureimagefromcameraandgallery/UseCustomActivity.java
+++ app/src/main/java/com/pritesh/androidcaptureimagefromcameraandgallery/UseCustomActivity.java
@@ -1,9 +1,12 @@
 package com.pritesh.androidcaptureimagefromcameraandgallery;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class UseCustomActivity extends Activity
 {
@@ -19,5 +22,11 @@ public class UseCustomActivity extends Activity
     {
         String text = view.getId() == R.id.view1 ? "Background" : "Foreground";
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+        SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
     }
 }
